4e6f3ade59f40b4b8a5dffa91bc59a12f5f13157
MVuko123 <vuko.marin52@gmail.com>
2022-04-05 19:46:03 +0200

    Rad na trećoj laboratorijskoj vježbi

---
+++ ToDo/app/src/main/java/com/example/todo/ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.todo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todo.R
+import com.example.todo.data.DataSource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dataSet = DataSource().loadTasks()
+        val recyclerView =  findViewById<RecyclerView>(R.id.recyclerView)
+        Log.d("TaskAdapter", "getItemCount: " + dataSet.size)
+
+        recyclerView.adapter =  TaskAdapter(this, dataSet)
+
+        //dodati edit text i button Add, i to ga dodaje na listu
+    }
+}
